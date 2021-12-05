ca4dbf6efb42af7d5b63ec1fa75587e366088ed5
PabatsoTejane <87855930+PabatsoTejane@users.noreply.github.com>
2021-12-06 00:01:50 +0200

    cache

--- base_app.py
+++ base_app.py
@@ -66,6 +66,7 @@ import plotly.graph_objects as go
 news_vectorizer = open("resources/tfidfvect.pkl","rb")
 tweet_cv = joblib.load(news_vectorizer) # loading your vectorizer from the pkl file
 
+@st.cache
 def get_data(filename):
 	data = pd.read_csv(filename)
 
