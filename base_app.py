f25608c44336c0f5057875976c5c20e35e4a88e5
PabatsoTejane <87855930+PabatsoTejane@users.noreply.github.com>
2021-12-06 19:42:14 +0200

    ...

--- base_app.py
+++ base_app.py
@@ -251,7 +251,7 @@ def main():
 				#text_lemma = tweet_cv.transform([text_lem]).toarray()
 				st.text("Original test ::\n{}".format(input_text))
 				text1 = cleaner(input_text) ###passing the text through the 'cleaner' function
-				text_lemma = text1 #tweet_cv.transform([text1]).toarray()
+				text_lemma = tweet_cv.transform([text1]).toarray()
 
 				if model_choice == 'Linear SVC':
 					predictor = load_prediction_models("LinearSVC.pkl")
