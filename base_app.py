c1056fe68d50ca4109c80e085609d48518cfbcb8
PabatsoTejane <87855930+PabatsoTejane@users.noreply.github.com>
2021-12-06 20:02:05 +0200

    ...

--- base_app.py
+++ base_app.py
@@ -276,9 +276,9 @@ def main():
 					predictor = load_prediction_models("SGDClassifier.pkl")
 					prediction = predictor.predict(text_lemma)
 					# st.write(prediction)
-					prediction_labels = {'Negative':-1,'Neutral':0,'Positive':1,'News':2}
-					final_result = get_keys(prediction,prediction_labels)
-					st.success("Tweet Categorized as:: {}".format(prediction))
+					#prediction_labels = {'Negative':-1,'Neutral':0,'Positive':1,'News':2}
+					#final_result = get_keys(prediction,prediction_labels)
+					st.success("Tweet Categorized as: {}".format(prediction))
 				#st.success("Text Categorized as: {}".format(prediction))
 
 			#if st.button("Classify"):
