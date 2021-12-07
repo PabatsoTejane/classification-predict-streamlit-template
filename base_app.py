f390cfcf3d124f5fbd3ef76755dd3f8d8463ce55
PabatsoTejane <87855930+PabatsoTejane@users.noreply.github.com>
2021-12-07 22:16:29 +0200

    about

--- base_app.py
+++ base_app.py
@@ -144,9 +144,9 @@ def main():
 	st.title("Climate Change Belief Tweet Classifier")
 	#st.image('images//DataDotIncLogo.png')
 	st.sidebar.subheader('About Data Dot Inc.')
-	st.sidebar.markdown('a consultancy firm that uses the data science process to solve your everyday marketing problem')
+	st.sidebar.markdown('A consultancy firm that uses the data science process to solve your everyday marketing problem')
 	#st.sidebar.image('images//DataDotIncLogo.png')
-
+	st.markdown('    ')
 	# Creating sidebar with selection box 
 	# you can create multiple pages this way
 	options = ["Summary", "EDA", "Prediction", "Information"]
