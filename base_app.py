2e561dd7776621103c47113092ca79c13f76708d
PabatsoTejane <87855930+PabatsoTejane@users.noreply.github.com>
2021-12-08 00:42:44 +0200

    subheader

--- base_app.py
+++ base_app.py
@@ -193,7 +193,7 @@ def main():
 			st.write(tweets[['sentiment', 'message']]) # will write the cleaned df to the page
 
 	if selection == 'Prediction':
-		st.info('Classify your tweet here using the ML Models below')
+		st.subheader('Classify your tweet using our M achine Learning Models')
 		data_source = ['Select option', 'Data Frame'] # differentiating between a single text and a dataset input
 		source_selection = st.selectbox('What to classify?', data_source)
 
