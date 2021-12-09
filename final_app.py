5e7084e8c344779de9be671313374ca56e6842ff
PabatsoTejane <87855930+PabatsoTejane@users.noreply.github.com>
2021-12-09 16:30:36 +0200

    ntlk

--- final_app.py
+++ final_app.py
@@ -28,6 +28,7 @@ import joblib,os
 # Libraries to be used in data cleaning and model
 import nltk
 nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
 from nltk.tokenize import word_tokenize
 from nltk import TreebankWordTokenizer
 from nltk.stem import WordNetLemmatizer
